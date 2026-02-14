dff5470358e0401b3f92993b277fd708a1bd94ce
Ben Smith <ben@hellosmithy.com>
2026-02-14 14:31:53 +0000

    fix footer styles

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,21 +1,27 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
+import {
+  QuartzComponent,
+  QuartzComponentConstructor,
+  QuartzComponentProps,
+} from "./types";
+import style from "./styles/footer.scss";
 
 interface Options {
-  links: Record<string, string>
+  links: Record<string, string>;
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
+    const links = opts?.links ?? {};
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Built with <a href="https://quartz.jzhao.xyz/">Quartz</a> ·{" "}
+          <a
+            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            rel="license"
+          >
+            CC BY-NC-SA
+          </a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
@@ -25,9 +31,9 @@ export default ((opts?: Options) => {
           ))}
         </ul>
       </footer>
-    )
-  }
+    );
+  };
 
-  Footer.css = style
-  return Footer
-}) satisfies QuartzComponentConstructor
+  Footer.css = style;
+  return Footer;
+}) satisfies QuartzComponentConstructor;
